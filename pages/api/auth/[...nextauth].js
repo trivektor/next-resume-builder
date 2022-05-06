4ce657ff727156b2163040a75a7046436eb8aeb4
Tri Vuong <trivektor@gmail.com>
2022-05-06 13:08:07 +0700

    add Github provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,15 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 import LinkedinProvider from "next-auth/providers/linkedin";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
